c7e519aa293b3296570940dd206ff5fc40e0616c
Victoria Bennett <134219248+Victoria1232@users.noreply.github.com>
2023-08-16 10:29:42 -0600

    Role.java

--- src/java/models/Role.java
+++ src/java/models/Role.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package models;
 
 import java.io.Serializable;
@@ -22,7 +17,7 @@ import javax.xml.bind.annotation.XmlTransient;
 
 /**
  *
- * @author awarsyle
+ * @author Victo
  */
 @Entity
 @Table(name = "role")
